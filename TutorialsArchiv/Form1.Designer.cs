488f49a49ea58d9c22c47e0ff09c689c4684e0c0
Jan (LernMoment.de) <jan@lernmoment.de>
2019-06-19 15:12:02 +0200

    Erlaubt EIngabe von Titel und URL

--- TutorialsArchiv/Form1.Designer.cs
+++ TutorialsArchiv/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.titelTextBox = new System.Windows.Forms.TextBox();
+            this.titelLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(12, 61);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(32, 13);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "URL:";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(50, 58);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(100, 20);
+            this.urlTextBox.TabIndex = 3;
+            // 
+            // titelTextBox
+            // 
+            this.titelTextBox.Location = new System.Drawing.Point(50, 32);
+            this.titelTextBox.Name = "titelTextBox";
+            this.titelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.titelTextBox.TabIndex = 1;
+            // 
+            // titelLabel
+            // 
+            this.titelLabel.AutoSize = true;
+            this.titelLabel.Location = new System.Drawing.Point(12, 35);
+            this.titelLabel.Name = "titelLabel";
+            this.titelLabel.Size = new System.Drawing.Size(30, 13);
+            this.titelLabel.TabIndex = 2;
+            this.titelLabel.Text = "Titel:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(61, 84);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Speichern";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(211, 127);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.titelTextBox);
+            this.Controls.Add(this.titelLabel);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.urlLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.TextBox titelTextBox;
+        private System.Windows.Forms.Label titelLabel;
+        private System.Windows.Forms.Button saveButton;
     }
 }
 
