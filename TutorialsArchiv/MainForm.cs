810e20b20549edc1851c222fa11d87384072b5a3
Jan (LernMoment.de) <jan@lernmoment.de>
2019-06-30 16:32:31 +0200

    Verwendet eigenen Zustand für neue Ressource
    Die Behandlung einer neu hinzugefügten Ressource ist nach den definierten Zuständen anders als eine bestehende Ressource. Daher gibt es nun einen separaten Zustand dafür und alle Aktionen für einen neue Ressource laufen nun rund.

--- TutorialsArchiv/MainForm.cs
+++ TutorialsArchiv/MainForm.cs
@@ -13,16 +13,17 @@ namespace TutorialsArchiv
 {
     public partial class MainForm : Form
     {
-        enum EntryMode
+        enum UiMode
         {
-            NoSelection = 0,
-            ResourceSelected,
-            UserEditsResource
+            Init = 0,
+            UserSelectedExistingResource,
+            UserEditsExistingResource,
+            UserEditsNewResource
         }
 
         private readonly FileDatabase _db = null;
         private readonly List<TeachingResource> _allResources = null;
-        private EntryMode _mode = EntryMode.NoSelection;
+        private UiMode _mode = UiMode.Init;
         private TeachingResource _activeResource = null;
 
         public MainForm()
@@ -35,38 +36,29 @@ namespace TutorialsArchiv
         private void MainForm_Load(object sender, EventArgs e)
         {
             _allResources.AddRange(_db.LoadAllEntries());
-            EnterNoSelectionMode();
+            EnterInitMode();
             RefreshDGV();
         }
 
-        private void EnterNoSelectionMode()
+        private void EnterInitMode()
         {
-            if (_mode == EntryMode.UserEditsResource)
-            {
-                throw new InvalidOperationException("Wir befinden uns im Editiermodus. Das darf jetzt nicht sein!");
-            }
-
-            _mode = EntryMode.NoSelection;
+            _mode = UiMode.Init;
             _activeResource = null;
 
             cancelButton.Enabled = false;
             deleteButton.Enabled = false;
             updateButton.Enabled = false;
+            createButton.Enabled = true;
 
             titelTextBox.Text = string.Empty;
             urlTextBox.Text = string.Empty;
 
-            teachingResourcesDGV.Select(); // Der Detailbereich soll nicht den Fokus haben!
+            //teachingResourcesDGV.Select(); // Der Detailbereich soll nicht den Fokus haben!
         }
 
-        private void EnterSelectionMode(TeachingResource selectedResource)
+        private void EnterSelectExistingMode(TeachingResource selectedResource)
         {
-            if (_mode == EntryMode.UserEditsResource)
-            {
-                throw new InvalidOperationException("Wir befinden uns im Editiermodus. Das darf jetzt nicht sein!");
-            }
-
-            _mode = EntryMode.ResourceSelected;
+            _mode = UiMode.UserSelectedExistingResource;
             _activeResource = selectedResource;
 
             deleteButton.Enabled = true;
@@ -81,23 +73,27 @@ namespace TutorialsArchiv
 
         private void AllTextBoxes_KeyPress(object sender, KeyPressEventArgs e)
         {
-            EnterEditingMode();
+            // editing a new Resource is different than an editing an existing Resource
+            if (_mode != UiMode.UserEditsNewResource)
+            {
+                EnterEditExistingMode();
+            }
         }
 
-        private void EnterEditingMode()
+        private void EnterEditExistingMode()
         {
-            if (_mode == EntryMode.UserEditsResource)
+            if (_mode == UiMode.UserEditsExistingResource)
             {
                 return;
             }
             else if (_activeResource == null)
             {
-                MessageBox.Show(this, "Es wurde noch kein Eintrag zum Ändern ausgewählt oder hinzugefügt!");
-                EnterNoSelectionMode();
+                MessageBox.Show(this, "Es wurde noch kein Eintrag zum Ändern ausgewählt!");
+                EnterInitMode();
                 return;
             }
 
-            _mode = EntryMode.UserEditsResource;
+            _mode = UiMode.UserEditsExistingResource;
 
             deleteButton.Enabled = true;
             cancelButton.Enabled = true;
@@ -106,15 +102,41 @@ namespace TutorialsArchiv
 
         private void CreateButton_Click(object sender, EventArgs e)
         {
-            if (_mode == EntryMode.UserEditsResource)
+            if (_mode == UiMode.UserEditsExistingResource)
             {
                 MessageBox.Show("Bitte erst die Änderungen speichern oder verwerfen!");
                 return;
             }
 
-            _allResources.Add(new TeachingResource("Neue Ressource", "bitte ausfüllen"));
+            TeachingResource newResource = new TeachingResource("Neue Ressource", "bitte ausfüllen");
+            _allResources.Add(newResource);
             RefreshDGV();
             SelectLastRowInDGV();
+
+            EnterEditNewMode(newResource);
+        }
+
+        private void EnterEditNewMode(TeachingResource newResource)
+        {
+            if (_mode == UiMode.UserEditsExistingResource)
+            {
+                MessageBox.Show("Bitte erst die Änderungen speichern oder verwerfen!");
+                return;
+            }
+
+            _mode = UiMode.UserEditsNewResource;
+
+            _activeResource = newResource;
+
+            createButton.Enabled = false;
+            deleteButton.Enabled = false;
+            cancelButton.Enabled = true;
+            updateButton.Enabled = true;
+
+            titelTextBox.Text = _activeResource.Title;
+            urlTextBox.Text = _activeResource.Url;
+
+            titelTextBox.Select();
         }
 
         private void SelectLastRowInDGV()
@@ -132,21 +154,31 @@ namespace TutorialsArchiv
         private void RefreshDGV()
         {
             // HACK: JS, Our _allResources does currently not support data binding. Thus we need to improvise
-            //teachingResourcesDGV.RowEnter -= new DataGridViewCellEventHandler(TeachingResourcesDGV_RowEnter);
+            teachingResourcesDGV.RowEnter -= new DataGridViewCellEventHandler(TeachingResourcesDGV_RowEnter);
+            teachingResourcesDGV.ClearSelection();
             teachingResourcesDGV.DataSource = null;
             teachingResourcesDGV.Rows.Clear();
             teachingResourcesDGV.DataSource = _allResources;
             teachingResourcesDGV.Refresh();
-            teachingResourcesDGV.ClearSelection();
-            //teachingResourcesDGV.RowEnter += new DataGridViewCellEventHandler(TeachingResourcesDGV_RowEnter);
+            teachingResourcesDGV.RowEnter += new DataGridViewCellEventHandler(TeachingResourcesDGV_RowEnter);
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
-            // TODO: JS, should we ask user whether he is sure to delete the input?
-            _activeResource = null;
-            _mode = EntryMode.NoSelection;
-            EnterNoSelectionMode();
+            if (_mode == UiMode.UserEditsNewResource)
+            {
+                // TODO: JS, should we ask user whether he is sure to delete the input?
+                _allResources.RemoveAt(_allResources.Count - 1);
+                RefreshDGV();
+
+                _activeResource = null;
+                _mode = UiMode.Init;
+                EnterInitMode();
+            }
+            else
+            {
+                throw new InvalidOperationException("UI ist nicht im Edit-Modus und somit kann dieser auch nicht angebrochen werden!");
+            }
         }
 
         private TeachingResource GetCurrentlySelectedResource()
@@ -156,26 +188,40 @@ namespace TutorialsArchiv
 
         private void DeleteButton_Click(object sender, EventArgs e)
         {
-            TeachingResource resource = GetCurrentlySelectedResource();
-            _allResources.Remove(resource);
-            _db.Save(_allResources);
-            RefreshDGV();
-            ClearEntryUIElements();
+            if (_mode == UiMode.UserSelectedExistingResource)
+            {
+                TeachingResource resource = GetCurrentlySelectedResource();
+                _allResources.Remove(resource);
+                _db.Save(_allResources);
+                RefreshDGV();
+                EnterInitMode();
+            }
+            else
+            {
+                throw new InvalidOperationException("UI ist nicht im Resource-Ausgewählt-Modus! Nur darin kann gelöscht werden!");
+            }
         }
 
         private void UpdateButton_Click(object sender, EventArgs e)
         {
-            TeachingResource resource = GetCurrentlySelectedResource();
-            resource.Title = titelTextBox.Text;
-            resource.Url = urlTextBox.Text;
-            _db.Save(_allResources);
-            RefreshDGV();
+            if (_mode == UiMode.UserEditsNewResource || _mode == UiMode.UserEditsExistingResource)
+            {
+                _activeResource.Title = titelTextBox.Text;
+                _activeResource.Url = urlTextBox.Text;
+                _db.Save(_allResources);
+                RefreshDGV();
+                EnterInitMode();
+            }
+            else
+            {
+                throw new InvalidOperationException("UI ist nicht im Edit-Modus und somit kann dieser auch nicht angebrochen werden!");
+            }
         }
 
         private void TeachingResourcesDGV_RowEnter(object sender, DataGridViewCellEventArgs e)
         {
             TeachingResource selectedResource = teachingResourcesDGV.Rows[e.RowIndex].DataBoundItem as TeachingResource;
-            EnterSelectionMode(selectedResource);
+            EnterSelectExistingMode(selectedResource);
         }
     }
 }
