ca8355edb9462928c0102a64a0cf76048de88e71
Jan (LernMoment.de) <jan@lernmoment.de>
2019-07-05 23:43:24 +0200

    Vereinfacht schreiben der CSV Datei
    Auch wenn ich etwas gebraucht habe um CsvHelper zu verstehen, wird das Design dadurch wesentlich besser. Außerdem sollte es so möglich sein die Tags ohne Probleme einzufügen.

--- TutorialsArchiv/FileDatabase.cs
+++ TutorialsArchiv/FileDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,53 +19,29 @@ namespace TutorialsArchiv
 
         public void Save(IEnumerable<TeachingResource> entries)
         {
-            if (File.Exists(_fileName))
+            using (var writer = new StreamWriter(_fileName))
+            using (var csv = new CsvWriter(writer))
             {
-                File.Delete(_fileName);
+                csv.Configuration.Delimiter = ";";
+                csv.Configuration.HasHeaderRecord = false;
+
+                csv.WriteRecords(entries);
+                writer.Flush();
             }
-
-            List<string> csvLines = ConvertToCsvLines(entries);
-
-            File.AppendAllLines(_fileName, csvLines);
-        }
-
-        public TeachingResource LoadFirstEntry()
-        {
-            if (File.Exists(_fileName))
-            {
-                IEnumerable<string> entries = File.ReadLines(_fileName);
-                return TeachingResource.BuildFromCsvLine(entries.First());
-            }
-
-            return null;
         }
 
         public IEnumerable<TeachingResource> LoadAllEntries()
         {
             List<TeachingResource> allResources = new List<TeachingResource>();
 
-            if (File.Exists(_fileName))
+            using (var reader = new StreamReader(_fileName))
+            using (var csv = new CsvReader(reader))
             {
-                IEnumerable<string> csvLines = File.ReadLines(_fileName);
-                foreach (string entry in csvLines)
-                {
-                    allResources.Add(TeachingResource.BuildFromCsvLine(entry));
-                }
+                csv.Configuration.HasHeaderRecord = false;
+                allResources = csv.GetRecords<TeachingResource>().ToList();
             }
 
             return allResources;
         }
-
-        private static List<string> ConvertToCsvLines(IEnumerable<TeachingResource> entries)
-        {
-            List<string> csvLines = new List<string>();
-            foreach (var item in entries)
-            {
-                csvLines.Add(item.ToCsvLine());
-            }
-
-            return csvLines;
-        }
-
     }
 }
--- TutorialsArchiv/TeachingResource.cs
+++ TutorialsArchiv/TeachingResource.cs
@@ -29,25 +29,14 @@ namespace TutorialsArchiv
         public MediumType Medium { get; set; }
         public TargetAudience Audience { get; set; }
 
+        public TeachingResource() { }
+
         public TeachingResource(string theTitle, string theUrl, MediumType theMedium, TargetAudience theAudience)
         {
             Title = theTitle;
             Url = theUrl;
             Medium = theMedium;
             Audience = theAudience;
-        }
-
-        public string ToCsvLine()
-        {
-            return $"{Title};{Url};{Medium.ToString()};{Audience.ToString()}";
-        }
-
-        public static TeachingResource BuildFromCsvLine(string csvLine)
-        {
-            string[] elements = csvLine.Split(';');
-            Enum.TryParse(elements[2], out MediumType medium);
-            Enum.TryParse(elements[3], out TargetAudience audience);
-            return new TeachingResource(elements[0], elements[1], medium, audience);
         }
     }
 }
